f3cd349bb0c5fa96264dfe525c5054d58c33dc40
gaoxuxu123 <394703554@qq.com>
2016-03-02 09:56:44 +0800

    README.md

--- README.md
+++ README.md
@@ -3,6 +3,49 @@
 ```
 
  composer require yii-smarty/smarty dev-master
-  
+
+ 或者在composer.json中加入
+
+ "require": {
+
+        "yii-smarty/smarty": "dev-master"
+ }
 
 ```
+
+ 更新依赖 ``` composer update ```
+
+ ##使用说明
+
+ ```
+     必须在views文件夹下面创建2个文件夹分别是template_cache和templates
+     用来存放自己的视图文件，当然也可以自己修改自定义
+
+     源代码中：
+
+            public $SMARTY_TMPDIR   = '/views/templates/';
+
+             public $SMARTY_CONFIG   = '/views/templates/tpl_conf/';
+
+             public $SMARTY_CACHEDIR = '/views/template_cache/';
+
+             public $SMARTY_COMPILE  = '/views/webdata/tpl_compile/';
+
+             public $LIFTTIME        = 1800;
+
+             public $SMARTY_DLEFT    = '<{';
+
+             public $SMARTY_DRIGHT   = '}>';
+
+     上述参数都可自定义。
+
+
+    在控制器中引入该类 : ```use Yxu\SmartyLib\CSmarty;```
+    //实例对象
+    $smarty =  CSmarty::getInstance();
+    //传递参数
+    $smarty->assign('name','HelloSmarty');
+    //展示的界面 路径views/templates/index.html
+    $smarty->display("index.html");
+
+ ```
--- src/CSmarty.php
+++ src/CSmarty.php
@@ -1,41 +1,50 @@
 <?php 
 
      namespace Yxu\SmartyLib;
-	 define("BASEPATH",\Yii::$app->basePath);
      require_once "smarty/Smarty.class.php";
-     define('SMARTY_TMPDIR',BASEPATH.'/views/templates/');
-     define('SMARTY_CONFIG',BASEPATH.'/views/templates/tpl_conf/');
-     define('SMARTY_CACHEDIR',BASEPATH.'/views/template_cache/');
-     define('SMARTY_COMPILE',BASEPATH.'/views/webdata/tpl_compile/');
-     define('LIFTTIME',1800);
-     define('SMARTY_DLEFT', '<{');
-     define('SMARTY_DRIGHT', '}>');
+	 define("BASEPATH",\Yii::$app->basePath);
     class CSmarty extends \Smarty{
 
         protected static $_instance = NULL;
 
-        static function getInstance(){
+        public $SMARTY_TMPDIR   = '/views/templates/';
+
+        public $SMARTY_CONFIG   = '/views/templates/tpl_conf/';
+
+        public $SMARTY_CACHEDIR = '/views/template_cache/';
+
+        public $SMARTY_COMPILE  = '/views/webdata/tpl_compile/';
+
+        public $LIFTTIME        = 1800;
+
+        public $SMARTY_DLEFT    = '<{';
+
+        public $SMARTY_DRIGHT   = '}>';
+
+
+        public static function getInstance(){
+
             if(self::$_instance == NULL){
+
                 self::$_instance = new CSmarty();
             }
             return self::$_instance;
         }
     
-        function __construct(){
+        public function __construct(){
+
             parent::__construct();
 
-            $this->template_dir = SMARTY_TMPDIR;
-            $this->compile_dir = SMARTY_COMPILE;
-            $this->config_dir = SMARTY_CONFIG;
-            $this->compile_check = true;
-            $this->caching = 1;
-            $this->cache_dir = SMARTY_CACHEDIR;
-            $this->left_delimiter  =  SMARTY_DLEFT;
-            $this->right_delimiter =  SMARTY_DRIGHT;
-            $this->cache_lifetime = LIFTTIME;
-           
-        }
-        function init(){
+            $this->template_dir     = BASEPATH.$this->SMARTY_TMPDIR;
+            $this->compile_dir      = BASEPATH.$this->SMARTY_COMPILE;
+            $this->config_dir       = BASEPATH.$this->SMARTY_CONFIG;
+            $this->compile_check    = true;
+            $this->caching          = 1;
+            $this->cache_dir        = BASEPATH.$this->SMARTY_CACHEDIR;
+            $this->left_delimiter   = BASEPATH.$this->SMARTY_DLEFT;
+            $this->right_delimiter  = BASEPATH.$this->SMARTY_DRIGHT;
+            $this->cache_lifetime   = BASEPATH.$this->LIFTTIME;
 
         }
+
      }
